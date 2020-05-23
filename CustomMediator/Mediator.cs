e836d2a48eded353f950410bf6d43be9167c8828
alex.ford <alex.ford@efoodhandlers.com>
2020-05-23 13:16:31 -0700

    Init commit.

---
+++ CustomMediator/Mediator.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace CustomMediator
+{
+     public class Mediator
+    {
+        // Invoke commands with no return types.
+        public void Send(IRequest request)
+        {
+            var    commandType = request.GetType();
+            var    handlerType = typeof(IRequestHandler<>).MakeGenericType(commandType);
+            object handler;
+
+            try
+            {
+                handler = RequestHandlerFactory.CreateRequestHandler(handlerType);
+            }
+            catch
+            {
+                throw new Exception(string.Format("Handler for type {0} not found.", commandType));
+            }
+
+            var handle = handler.GetType().GetMethod("Handle");
+
+            try
+            {
+                ValidateRequest(request);
+                handle.Invoke(handler, new object[] {request});
+            }
+            catch (Exception exception)
+            {
+                HandleException(exception.InnerException ?? exception, request, handler.GetType());
+                throw exception.InnerException ?? exception;
+            }
+        }
+
+        // Invoke queries with return types.
+        public TResponse Send<TResponse>(IRequest<TResponse> request)
+        {
+            var    commandType = request.GetType();
+            var    handlerType = typeof(IRequestHandler<,>).MakeGenericType(commandType, typeof(TResponse));
+            object handler;
+
+            try
+            {
+                handler = RequestHandlerFactory.CreateRequestHandler(handlerType);
+            }
+            catch
+            {
+                throw new Exception(string.Format("Handler for type {0} not found.", commandType));
+            }
+
+            var handle = handler.GetType().GetMethod("Handle");
+
+            try
+            {
+                ValidateRequest(request);
+                var result = handle.Invoke(handler, new object[] {request});
+
+                return (TResponse) result;
+            }
+            catch (Exception exception)
+            {
+                HandleException(exception.InnerException ?? exception, request, handler.GetType());
+                throw exception.InnerException ?? exception;
+            }
+        }
+
+        private void HandleException(Exception exception, object request, Type handlerType)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ValidateRequest(object request)
+        {
+            ValidationContext             vc      = new ValidationContext(request);
+            ICollection<ValidationResult> results = new List<ValidationResult>();
+            bool                          isValid = Validator.TryValidateObject(request, vc, results, true);
+
+            if (!isValid)
+            {
+                throw new Exception(string.Join(" ", results.Select(x => x.ErrorMessage)));
+            }
+        }
+    }
+
+
+    public interface IRequestHandler<in TCommand, out TResponse> where TCommand : IRequest<TResponse>
+    {
+        TResponse Handle(TCommand request);
+    }
+
+    public interface IRequestHandler<in TCommand>
+    {
+        void Handle(TCommand request);
+    }
+    
+    public interface IRequest<out TResponse>
+    {
+        
+    }
+
+    public interface IRequest
+    {
+    }
+}
---
+++ CustomMediator/RequestHandlerFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomMediator
+{
+    public static class RequestHandlerFactory
+    {
+        private static List<Type> _handlers = new List<Type>();
+
+        public static void AddRequestHandler(Type requestHandlerType)
+        {
+            _handlers.Add(requestHandlerType);
+        }
+
+        public static object CreateRequestHandler(Type genericType)
+        {
+            var handlerType = _handlers.FirstOrDefault(x => x.GetInterfaces().Contains(genericType));
+            
+            return Activator.CreateInstance(handlerType);
+        }
+    }
+}
